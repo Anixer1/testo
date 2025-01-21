3f4e8d5d044e592faaa61302e0aea89854e93944
Anixer1 <165531263+Anixer1@users.noreply.github.com>
2025-01-22 01:00:11 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js
-const api = 'https://aniwatch-api-net.vercel.app/api/v2/hianime/home'; // Replace with your API URL
+const api = 'https://streaming23.vercel.app/api/v2/hianime/home'; // Replace with your API URL
